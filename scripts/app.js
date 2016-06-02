3a04cba34ed6edb9928b74ce238f4fcd89d05857
Umar Waliyullah <umar.mw@gmail.com>
2016-06-02 16:50:09 +0400

    Implement editing

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,14 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("hello world !!");
   };
+
+  $scope.learningNgChange = function() {
+    console.log("an input has change !!");
+  };
+
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "water the dog"},
+    {"name": "feed the lawn"},
+  ]
 });
