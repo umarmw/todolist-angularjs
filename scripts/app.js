1f2b7aab9730c31cab2b9833ebe35e371944bd1e
Umar Waliyullah <umar.mw@gmail.com>
2016-06-02 18:26:57 +0400

    add new functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,26 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope, dataService) {
 
+  $scope.addTodo =  function(){
+    var todo = {name: "This is a new todo."};
+    $scope.todos.push(todo);
+  }
+
   $scope.helloConsole = dataService.helloConsole;
 
   dataService.getTodos(function(response){
     console.log(response.data);
     $scope.todos =  response.data;
   });
+
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  };
 
   $scope.helloWorld = function() {
     console.log("hello world !!");
@@ -25,5 +39,13 @@ angular.module("todoListApp", [])
   this.getTodos = function(callback){
     $http.get('mock/todos.json')
     .then(callback);
+  }
+  this.deleteTodo = function(todo){
+    console.log("The "+todo.name+" todo has been deleted!");
+    //other db logic
+  }
+  this.saveTodo = function(todo){
+    console.log("The "+todo.name+" todo has been saved!");
+    //other db logic
   }
 });
